db8621d13c96067eaa3e22bf52fdaa6635a53d01
Rohit Dutta <91669680+rohitTo95@users.noreply.github.com>
2023-09-12 21:20:45 +0530

    Update CahtBot.py

--- CahtBot.py
+++ CahtBot.py
@@ -1,11 +1,10 @@
 import openai
 import os
 import json
-import webbrowser
-from youtube_search import YoutubeSearch
-import  Voice_Assistant
+import pywhatkit
+import Voice_Assistant  # Correct the import if needed
 
-# Load API key and prompt from config.json
+# Load API key and prompt from config file
 current_file = os.path.abspath(__file__)
 current_directory = os.path.dirname(current_file)
 config_file_path = os.path.join(current_directory, 'config.json')
@@ -26,27 +25,16 @@ except json.JSONDecodeError:
 
 openai.api_key = api_key
 
-user_message_list = []
-response_message_list = []
-
-
-def play_youtube_video(search_query):
-    results = YoutubeSearch(search_query, max_results=1).to_dict()
-    if results:
-        video_id = results[0]['id']
-        video_url = f"https://www.youtube.com/watch?v={video_id}"
-        webbrowser.open(video_url)
-    else:
-        print("No videos found.")
-
-
 def chatBot(input_of_user):
+    user_message_list = []  # Moved inside the function to reset for each interaction
+    response_message_list = []  # Moved inside the function to reset for each interaction
+
     if input_of_user.startswith("/play "):
         video_name = input_of_user[6:]  # Extract the video name
-        play_youtube_video(video_name)
+        pywhatkit.playonyt(video_name)
         return ''
     elif input_of_user.lower() == "/spring assistant mode":
-         Voice_Assistant.start_voice_assistant()
+        Voice_Assistant.start_voice_assistant()  # Remove the extra indent
         return "Voice Assistant mode activated. You can now use voice commands."
     else:
         user_message_list.append({'role': 'user', 'content': prompt})
@@ -61,7 +49,6 @@ def chatBot(input_of_user):
         chat_response = user_query['choices'][0]['message']['content']
         response_message_list.append({'role': 'assistant', 'content': chat_response})
         return chat_response
-
 
 print('Welcome To Spring, an AI ChatBot Song Recommender. Ready to chat? (Commands: /Play {Song Name}, /Spring assistant mode)')
 while True:
